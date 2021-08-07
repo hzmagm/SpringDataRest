70650e120cb9cde80604e6dbed085c75e36723fc
hamza Sibella <hamzasibella@gmail.com>
2021-08-07 01:51:36 +0100

    first commit

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,1 @@
+INSERT INTO EMPLOYE(id,nom,cours) VALUES (10, 'Ali', 'Java');
---
+++ src/test/java/com/javainuse/test/SpringDataRest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javainuse.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
